2cde30c0014c14b256c6ad8efd5377000ea8d697
vladyslavdanko391-lab <vladyslavdanko391@gmail.com>
2026-09-21 15:36:46 +0200

    Correct length check in calcAverageCalories function
    Fix typo in length property check for days array.

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,8 @@
 function calcAverageCalories (days) {
+    if (days.lenght === 0) {
+        return 0;
+    }
+    
     let totalCalories = 0;
     for (const day of days) {
         totalCalories += day.calories;
